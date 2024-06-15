4521a7d27ae078901e48859bae3073613b2b816b
qwertzys <greg3ananda@gmail.com>
2024-06-15 10:07:57 +0700

    PUZZLE IS NOW FULLY FUNCTIONAL (IM FREE)

--- homePage/homeScript.js
+++ homePage/homeScript.js
@@ -1,61 +1,62 @@
 var row = 4;
-var column = 4;
+var col = 4;
 
 var currTile;
 var otherTile;
 
-window.load = function(){
+window.onload = function() {
     // initialize board
-    for (let i = 0; i < row; i++){
-        for (let j = 0; j < column; j++){
+    for (let r = 0; r < row; r++){
+        for (let c = 0; c < col; c++){
             let tile = document.createElement("img");
-            tile.src = "/puzzleImage/blank.jpg";// Insert image here
+            tile.src = "./puzzleImages/blank.jpg";
 
-                // Drag functionality
-            tile.addEventListener("dragstart", dragStart); // click on mg to drag
-            tile.addEventListener("dragover", dragOver); // drag img
-            tile.addEventListener("dragenter", dragEnter); // drag img to another
-            tile.addEventListener("dragleave", dragLeavet); // drag img away from another
-            tile.addEventListener("dragdrop", dragDrop); // drop img
-            tile.addEventListener("dragend", dragEnd); // after dropping, end state
+            // Drag Functionalities
+            tile.addEventListener("dragstart", dragStart); // click on img to drag
+            tile.addEventListener("dragover", dragOver); // drag on img
+            tile.addEventListener("dragenter", dragEnter); // drag img into another
+            tile.addEventListener("dragleave", dragLeave); // drag img away from another
+            tile.addEventListener("drop", dragDrop); // drop image on another
+            tile.addEventListener("dragend", dragEnd); // after completing dragDrop
+        
+            
             document.getElementById("board").append(tile);
         }
     }
+
+    // pieces
+    let pieces = [];
+    for (let i=1;i<=row*col;i++){
+        pieces.push(i.toString());
+    }
+
+    // Shuffle piece
+    pieces.reverse();
+    for (let i=0; i < pieces.length;i++){
+        let j = Math.floor(Math.random() *pieces.length);
+        
+        let tmp = pieces[i];
+        pieces[i] = pieces[j];
+        pieces[j] = tmp;
+    }
+
+    // load pieces
+    for (let i = 0;i<pieces.length;i++){
+        let tile = document.createElement("img");
+        tile.src = "./puzzleImages/" + pieces[i] + ".jpg";
+
+        // Drag Functionalities
+        tile.addEventListener("dragstart", dragStart); // click on img to drag
+        tile.addEventListener("dragover", dragOver); // drag on img
+        tile.addEventListener("dragenter", dragEnter); // drag img into another
+        tile.addEventListener("dragleave", dragLeave); // drag img away from another
+        tile.addEventListener("drop", dragDrop); // drop image on another
+        tile.addEventListener("dragend", dragEnd); // after completing dragDrop
+
+        document.getElementById("pieces").append(tile);
+    }
 }
 
-// pieces
-let pieces = [];
-// initialize pieces into array
-for (let i=1;i <= rows*columns; i++){
-    pieces.push(i.toString());
-}
-
-// Shuffle pieces
-pieces.reverse();
-for (let i=0;i<pieces.length;i++){
-    let j=Math.floor(Math.random() * pieces.length);
-
-    // swap index
-    let tmp = pieces[i];
-    pieces[i] = pieces[j];
-    pieces[j] = tmp;
-}
-// Load Pieces
-for (let i=0;i<pieces.length;i++){
-    let tile = document.createElement("img");
-    tile.src = "/puzzleImages/blank.jpg" // Place holder, later add iamge based on number
-    
-    // Drag functionality
-    tile.addEventListener("dragstart", dragStart); // click on mg to drag
-    tile.addEventListener("dragover", dragOver); // drag img
-    tile.addEventListener("dragenter", dragEnter); // drag img to another
-    tile.addEventListener("dragleave", dragLeavet); // drag img away from another
-    tile.addEventListener("dragdrop", dragDrop); // drop img
-    tile.addEventListener("dragend", dragEnd); // after dropping, end state
-    document.getElementById("pieces").append(tile);
-}
-
-// Drag Functions
 function dragStart(){
     currTile = this;
 }
@@ -82,15 +83,6 @@ function dragEnd(){
     }
     let currImg = currTile.src;
     let otherImg = otherTile.src;
-
-    // Swap
     currTile.src = otherImg;
-    otherImg.src = currImg;
+    otherTile.src = currImg;
 }
-
-function isSolved(){
-    if(currTile.src.isSolved){
-        
-    }
-}
-
